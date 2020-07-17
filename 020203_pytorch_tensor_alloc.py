947a5b17b0d2b1543f8e67d122749f55fd09b609
황현동 <h2d2002@naver.com>
2020-07-17 22:38:51 +0900

    m

--- 020203_pytorch_tensor_alloc.py
+++
@@ -1,5 +0,0 @@
-import myutil as mu
-import torch
-
-t = torch.FloatTensor([0., 1., 2., 3., 4., 5., 6.])
-print(t)
--- 0202_tensor_mani.py
+++ 0202_tensor_mani.py
@@ -1,5 +1,9 @@
 import myutil as mu
 import numpy as np
+import torch
+
+################################################################################
+# 넘파이로 텐서 만들기(벡터와 행렬 만들기)
 
 t = np.array([0., 1., 2., 3., 4., 5., 6.])
 mu.log("t", t)
@@ -15,3 +19,50 @@ t = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.], [10., 11., 12.]])
 mu.log("t", t)
 mu.log("t.ndim", t.ndim)
 mu.log("t.shape", t.shape)
+
+t = torch.FloatTensor([0., 1., 2., 3., 4., 5., 6.])
+mu.log("t", t)
+mu.log("t.ndim", t.ndim)
+mu.log("t.shape", t.shape)
+
+################################################################################
+# 파이토치 텐서 선언하기(PyTorch Tensor Allocation)
+
+t = torch.FloatTensor([[1., 2., 3.],
+                       [4., 5., 6.],
+                       [7., 8., 9.],
+                       [10., 11., 12.]
+                       ])
+mu.log("t", t)
+mu.log("t.ndim", t.ndim)
+mu.log("t.shape", t.shape)
+
+################################################################################
+# 행렬 곱셈과 곱셈의 차이(Matrix Multiplication Vs. Multiplication)
+
+m1 = torch.FloatTensor([[1, 2], [3, 4]])
+m2 = torch.FloatTensor([[1], [2]])
+mu.log("m1", m1)
+mu.log("m2", m2)
+mu.log("m1 * m2", m1 * m2)
+mu.log("m1.matmul(m2)", m1.matmul(m2))
+
+################################################################################
+# 평균(Mean)
+
+t = torch.FloatTensor([1, 2])
+mu.log("t.mean()", t.mean())
+
+t = torch.FloatTensor([[1, 2], [3, 4]])
+mu.log("t", t)
+mu.log("t.mean(dim=0)", t.mean(dim=0))
+mu.log("t.mean(dim=1)", t.mean(dim=1))
+
+################################################################################
+# 최대(Max)와 아그맥스(ArgMax)
+t = torch.FloatTensor([[1, 2], [3, 4]])
+mu.log("t", t)
+mu.log("t.max()", t.max())
+mu.log("t.max(dim=0)", t.max(dim=0))
+mu.log("t.max(dim=1)", t.max(dim=1))
+mu.log("t.max(dim=-1)", t.max(dim=-1))
