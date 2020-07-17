b2e15235ec86d15a17c97bcb182001552cb9e9e3
HyundongHwang <hyundong.hwang@navercorp.com>
2020-07-17 15:47:43 +0900

    m

---
+++ 020203_pytorch_tensor_alloc.py
@@ -0,0 +1,5 @@
+import myutil as mu
+import torch
+
+t = torch.FloatTensor([0., 1., 2., 3., 4., 5., 6.])
+print(t)
--- 0202_tensor_mani.py
+++ 0202_tensor_mani.py
@@ -2,7 +2,6 @@ import myutil as mu
 import numpy as np
 
 t = np.array([0., 1., 2., 3., 4., 5., 6.])
-
 mu.log("t", t)
 mu.log("t.ndim", t.ndim)
 mu.log("t.shape", t.shape)
@@ -11,3 +10,8 @@ mu.log("t[2:5]", t[2:5])
 mu.log("t[4:-1]", t[4:-1])
 mu.log("t[:2]", t[:2])
 mu.log("t[3:]", t[3:])
+
+t = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.], [10., 11., 12.]])
+mu.log("t", t)
+mu.log("t.ndim", t.ndim)
+mu.log("t.shape", t.shape)
