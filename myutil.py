aad3a6b1fb7710893605d9f1f1054cded021472f
HyundongHwang <hyundong.hwang@navercorp.com>
2020-07-19 09:08:23 +0900

    m

--- myutil.py
+++ myutil.py
@@ -1,2 +1,16 @@
+g_oldLogType = "none"  # none, single, multi
+
 def log(name, value):
-    print("{} : {}".format(name, value))
+    global g_oldLogType
+    valueStr = "{}".format(value)
+
+    if "\n" in valueStr:
+        if g_oldLogType is "multi":
+            print("")
+        g_oldLogType = "multi"
+        print("{} : ".format(name))
+        print("    {}".format(valueStr))
+        print("")
+    else:
+        g_oldLogType = "single"
+        print("{} : {}", name, valueStr)
