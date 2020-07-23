c7e7d9cb690e84fc2a529e49a127b6bd74be10c5
HyundongHwang <h2d2002@naver.com>
2020-07-23 17:45:06 +0900

    m

--- 0304_multi_linear_regression_nn_module.py
+++ 0304_multi_linear_regression_nn_module.py
@@ -1,5 +1,8 @@
 ################################################################################
-# 다중 선형 회귀(Multivariable Linear regression)
+# - 다중 선형 회귀
+#   - 이제 데이터를 선언합니다.
+#   - 아래 데이터는 y=2x를 가정된 상태에서 만들어진 데이터로 우리는 이미 정답이 W=2, b=0임을 알고 있는 사태입니다.
+#   - 모델이 이 두 W와 b의 값을 제대로 찾아내도록 하는 것이 목표입니다.
 
 import myutil as mu
 import torch
@@ -14,7 +17,7 @@ x_train = torch.FloatTensor([[1], [2], [3]])
 y_train = torch.FloatTensor([[2], [4], [6]])
 
 model = nn.Linear(1, 1)
-mu.log("list(model.parameters())", list(model.parameters()))
+mu.log_model("model", model)
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 nb_epochs = 2000
 
@@ -33,3 +36,9 @@ for epoch in range(nb_epochs + 1):
             cost.item(),
             mu.model_to_str(model)
         ))
+
+mu.log_model("model", model)
+new_var = torch.FloatTensor([[4.0]])
+pred_y = model(new_var)
+mu.log("new_var.item()", new_var.item())
+mu.log("pred_y.item()", pred_y.item())
