64479f1d8964e181dedbc95225f54c7d8d9f1585
HyundongHwang <h2d2002@naver.com>
2020-07-23 12:02:42 +0900

    m

--- 0203_tensor_mani.py
+++ 0203_tensor_mani.py
@@ -61,7 +61,6 @@ mu.log("bt", bt)
 mu.log("bt.long()", bt.long())
 mu.log("bt.float()", bt.float())
 
-
 ################################################################################
 # 연결하기(concatenate)
 
@@ -69,3 +68,35 @@ x = torch.FloatTensor([[1, 2], [3, 4]])
 y = torch.FloatTensor([[5, 6], [7, 8]])
 mu.log("torch.cat([x, y], dim=0)", torch.cat([x, y], dim=0))
 mu.log("torch.cat([x, y], dim=1)", torch.cat([x, y], dim=1))
+
+################################################################################
+# 스택킹(Stacking)
+
+x = torch.FloatTensor([1, 4])
+y = torch.FloatTensor([2, 5])
+z = torch.FloatTensor([3, 6])
+mu.log("torch.stack([x, y, z])", torch.stack([x, y, z]))
+
+mu.log("torch.cat([x.unsqueeze(0), y.unsqueeze(0), z.unsqueeze(0)], dim=0)",
+       torch.cat([x.unsqueeze(0), y.unsqueeze(0), z.unsqueeze(0)], dim=0))
+
+mu.log("torch.stack([x, y, z], dim=1)", torch.stack([x, y, z], dim=1))
+
+################################################################################
+# ones_like와 zeros_like - 0으로 채워진 텐서와 1로 채워진 텐서
+
+x = torch.FloatTensor([[0, 1, 2], [2, 1, 0]])
+mu.log("x", x)
+mu.log("torch.ones_like(x)", torch.ones_like(x))
+mu.log("torch.zeros_like(x)", torch.zeros_like(x))
+
+################################################################################
+# In-place Operation (덮어쓰기 연산)
+
+x = torch.FloatTensor([[1, 2], [3, 4]])
+
+mu.log("x", x)
+mu.log("x.mul(2.)", x.mul(2.)) # 곱하기 2를 수행한 결과를 출력
+mu.log("x", x)
+mu.log("x.mul_(2.)", x.mul_(2.)) # 곱하기 2를 수행한 결과를 변수 x에 값을 저장하면서 결과를 출력
+mu.log("x", x)
