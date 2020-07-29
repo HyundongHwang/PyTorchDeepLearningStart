a7e1f87b465aea9a34102ddd331422983c7d83f6
HyundongHwang <h2d2002@naver.com>
2020-07-29 23:27:26 +0900

    m

--- 0504_softmax_regression.py
+++ 0504_softmax_regression.py
@@ -8,6 +8,14 @@ from torch.utils.data import DataLoader  # 데이터로더
 from torch.utils.data import Dataset
 import matplotlib.pyplot as plt  # 맷플롯립사용
 
+################################################################################
+# - 소프트맥스 회귀 구현하기
+#   - 이번 챕터에서는 소프트맥스 회귀를 로우-레벨과 F.cross_entropy를 사용해서 구현해보겠습니다.
+
+
+################################################################################
+# - 소프트맥스 회귀 구현하기(로우-레벨)
+
 torch.manual_seed(1)
 
 x_train = [[1, 2, 1, 1],
@@ -33,21 +41,100 @@ mu.log("y_one_hot", y_one_hot)
 
 W = torch.zeros((4, 3), requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
-
 optimizer = optim.SGD([W, b], lr=0.1)
-
 nb_epoches = 2000
 mu.plt_init()
 
 for epoch in range(nb_epoches + 1):
     hypothesis = F.softmax(x_train.matmul(W) + b, dim=1)
     cost = (y_one_hot * -torch.log(hypothesis)).sum().mean()
-
     optimizer.zero_grad()
     cost.backward()
     optimizer.step()
 
-    if epoch % 100 == 0 :
+    if epoch % 100 == 0:
         mu.log_epoch(epoch, nb_epoches, cost)
+
+mu.plt_show()
+
+################################################################################
+# - 소프트맥스 회귀 구현하기(하이-레벨)
+#   - 이제는 F.cross_entropy()를 사용하여 비용 함수를 구현해보겠습니다.
+#   - 주의할 점은 F.cross_entropy()는 그 자체로 소프트맥스 함수를 포함하고 있으므로
+#   - 가설에서는 소프트맥스 함수를 사용할 필요가 없습니다.
+
+
+W = torch.zeros((4, 3), requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+optimizer = optim.SGD([W, b], lr=0.1)
+nb_epoches = 2000
+mu.plt_init()
+
+for epoch in range(nb_epoches + 1):
+    z = x_train.matmul(W) + b
+    cost = F.cross_entropy(z, y_train)
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    if epoch % 100 == 0:
+        mu.log_epoch(epoch, nb_epoches, cost)
+
+mu.plt_show()
+
+################################################################################
+# - 소프트맥스 회귀 nn.Module로 구현하기
+#   - 이번에는 nn.Module로 소프트맥스 회귀를 구현해봅시다.
+#   - 선형 회귀에서 구현에 사용했던 nn.Linear()를 사용합니다.
+#   - output_dim이 1이었던 선형 회귀때와 달리 output_dim은 이제 클래스의 개수여야 합니다.
+
+
+model = nn.Linear(4, 3)
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+nb_epoches = 2000
+mu.plt_init()
+
+for epoch in range(nb_epoches + 1):
+    prediction = model(x_train)
+    cost = F.cross_entropy(prediction, y_train)
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    if epoch % 100 == 0:
+        mu.log_epoch(epoch, nb_epoches, cost, model=model)
+
+mu.plt_show()
+
+
+################################################################################
+# - 소프트맥스 회귀 클래스로 구현하기
+#   - 이제 소프트맥스 회귀를 nn.Module을 상속받은 클래스로 구현해봅시다.
+
+
+class SoftmaxClassificationModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(4, 3)
+
+    def forward(self, x):
+        res = self.linear(x)
+        return res
+
+
+model = SoftmaxClassificationModel()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+nb_epoches = 2000
+mu.plt_init()
+
+for epoch in range(nb_epoches + 1):
+    prediction = model(x_train)
+    cost = F.cross_entropy(prediction, y_train)
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    if epoch % 100 == 0:
+        mu.log_epoch(epoch, nb_epoches, cost, model=model)
 
 mu.plt_show()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# PyTorchDeepLearningStart
-PyTorchDeepLearningStart
+
+- 주피터노트북 동기화, git commit push
+
+```
+hhdpy jupyter_py_to_ipynb --skip-if-existed; git add *; git commit -m m; git push origin master;
+```
